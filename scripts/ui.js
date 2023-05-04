5c0838dbfb06084c464992cb4ee7dcfeaedb68fb
Tian Gao <gaogaotiantian@hotmail.com>
2023-05-03 18:57:48 -0700

    Fixed an attribute bug and updated readme

--- scripts/ui.js
+++ scripts/ui.js
@@ -402,7 +402,7 @@ UserProfileCard.prototype.updateData = function (data) {
         this.data["level"] = d["data"]["level"];
         this.data["title"] = d["data"]["official"]["title"];
         this.data["title_type"] = d["data"]["official"]["type"];
-        this.data["live_status"] = d["data"]["live_room"]["liveStatus"];
+        this.data["live_status"] = d["data"]["liveroom"] ? d["data"]["live_room"]["liveStatus"]: 0;
         this.data["vip"] = d["data"]["vip"]["status"];
         this.data["top_photo"] = d["data"]["top_photo"].replace("http://", "https://");
     } else if (data["api"] == "relation") {
