73c9f1ff8d7b302fc0291f531a15421915949337
F-park <52167622+F-park@users.noreply.github.com>
2024-01-06 15:09:24 +0800

    Fix default page when search returns empty (#116)

--- scripts/searchpage.js
+++ scripts/searchpage.js
@@ -44,15 +44,17 @@ function updatePage(clear=true) {
         let newUsers = [];
 
         for (let uid in noteData) {
-            if (!(this.users.has(uid))) {
-                let tags = getTags(uid);
-                if (keywords.every(keyword => tags.includes(keyword.replaceAll("#", "")))) {
-                    this.users.add(uid);
-                    newUsers.push(uid);
-                    if (newUsers.length >= 50) {
-                        // the API can take 50 users at most
-                        break;
-                    }
+            if (this.users.has(uid)) {
+                continue;
+            }
+
+            let tags = getTags(uid);
+            if (keywords.every(keyword => tags.includes(keyword.replaceAll("#", "")))) {
+                this.users.add(uid);
+                newUsers.push(uid);
+                if (newUsers.length >= 50) {
+                    // the API can take 50 users at most
+                    break;
                 }
             }
         }
@@ -67,7 +69,7 @@ function updatePage(clear=true) {
             myWrapper.appendChild(container);
         }
 
-        if (this.users.length == 0) {
+        if (this.users.size == 0) {
             myWrapper.innerHTML = `
             <div class="search-nodata-container p_relative">
                 <div class="no-data p_center text_center">
