c1af1048008a542faf88d2692817ff370d8065a0
F-park <52167622+F-park@users.noreply.github.com>
2024-01-29 12:55:25 +0800

    revert "remove `options/options.js`"

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,14 +1,3 @@
-chrome.storage.sync.get({
-    enableWordCloud: true,
-    minSize: 5
-}, function(items) {
-    biliScopeOptions = items;
-});
-
-function saveOptions() {
-    chrome.storage.sync.set(biliScopeOptions);
-}
-
 function titleTypeToClass(titleType) {
     if (titleType == 0) {
         return "biliscope-personal-auth-icon";
