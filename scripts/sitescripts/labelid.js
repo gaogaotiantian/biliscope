b28899ce25d2d8604dd07ceeb5485c969ee00a92
F-park <forest_park@foxmail.com>
2024-07-30 23:54:37 +0800

    更新 labelid.js

--- scripts/sitescripts/labelid.js
+++ scripts/sitescripts/labelid.js
@@ -107,10 +107,10 @@ function installIdHooks() {
         if (window.location.href.startsWith(BILIBILI_POPULAR_URL)) {
             labelPopularPage();
         } else if (window.location.href.startsWith(BILIBILI_DYNAMIC_URL) ||
-                   window.location.href.startsWith(BILIBILI_NEW_DYNAMIC_URL)) {
-            labelDynamicPage();
-        } else if (window.location.href.startsWith(BILIBILI_VIDEO_URL) ||
+                   window.location.href.startsWith(BILIBILI_NEW_DYNAMIC_URL) ||
                    window.location.href.startsWith(BILIBILI_SPACE_URL)) {
+            labelDynamicPage();
+        } else if (window.location.href.startsWith(BILIBILI_VIDEO_URL)) {
             labelVideoPage();
         }
     })
