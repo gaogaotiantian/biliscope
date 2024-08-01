af9e63cda7284ef180c9a34a935930180112bf58
F-park <52167622+F-park@users.noreply.github.com>
2024-08-01 22:18:23 +0800

    add user card display in dyn-orig-author

--- scripts/sitescripts/labelid.js
+++ scripts/sitescripts/labelid.js
@@ -49,6 +49,13 @@ function labelDynamicPage() {
         }
     }
 
+    for (const el of document.querySelectorAll(".dyn-orig-author__face, .dyn-orig-author__name")) {
+        const {uid} = el?._profile;
+        if (uid) {
+            el.setAttribute("biliscope-userid", uid);
+        }
+    }
+
     for (let el of document.getElementsByClassName("bili-dyn-up-list")) {
         let upList = el.__vue__.list;
         let upElements = el.getElementsByClassName("bili-dyn-up-list__item");
